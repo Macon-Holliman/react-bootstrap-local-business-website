3cbdbc71e260a66b1651ce6e89fab9433150384d
Macon Holliman <Maconholliman@gmail.com>
2020-02-25 16:51:56 -0600

    Added client FB link

--- src/components/Nav/FacebookSocial.js
+++ src/components/Nav/FacebookSocial.js
@@ -5,7 +5,7 @@ function FacebookSocial() {
   return (
     <div class="col  col-lg-3">
 
-    <a href="#">
+    <a href="https://www.facebook.com/groups/363191717888871/">
         <div class="facebook"></div>
     </a>
     </div>
